fd77d60f6b2620311e6a43f540565b0510115c26
mbattiti <A05331@bh.com.ar>
2025-05-14 15:04:08 -0300

    Initial Commit

---
+++ UalaCities/App/UalaCitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaCitiesApp.swift
+//  UalaCities
+//
+//  Created by MATIAS BATTITI on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaCitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ UalaCities/Features/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UalaCities
+//
+//  Created by MATIAS BATTITI on 14/05/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
