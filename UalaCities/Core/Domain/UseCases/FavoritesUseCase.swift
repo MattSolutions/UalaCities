fda4be02f07971af31692ecde8a9a4301192d702
mbattiti <A05331@bh.com.ar>
2025-05-14 15:40:37 -0300

    Added Search and Favorites UseCases

---
+++ UalaCities/Core/Domain/UseCases/FavoritesUseCase.swift
@@ -0,0 +1,52 @@
+//
+//  FavoritesUseCase.swift
+//  UalaCities
+//
+//  Created by MATIAS BATTITI on 14/05/2025.
+//
+
+import Foundation
+import Observation
+
+/// Use case for managing favorite cities
+protocol FavoritesUseCase {
+    /// The current set of favorite city IDs
+    var favorites: Set<Int> { get }
+    
+    /// Determines if a city is currently a favorite
+    func isFavorite(cityId: Int) -> Bool
+    
+    /// Changes the favorite status of a city
+    @discardableResult
+    func toggleFavorite(cityId: Int) -> Bool
+    
+    /// Returns only the favorite cities from a provided collection
+    func filterFavorites(cities: [City]) -> [City]
+}
+
+/// Default implementation
+@Observable
+class DefaultFavoritesUseCase: FavoritesUseCase {
+    private let repository: FavoritesRepository
+    
+    var favorites: Set<Int> {
+        repository.favorites
+    }
+    
+    init(repository: FavoritesRepository) {
+        self.repository = repository
+    }
+    
+    func isFavorite(cityId: Int) -> Bool {
+        repository.isFavorite(cityId: cityId)
+    }
+    
+    @discardableResult
+    func toggleFavorite(cityId: Int) -> Bool {
+        repository.toggleFavorite(cityId: cityId)
+    }
+    
+    func filterFavorites(cities: [City]) -> [City] {
+        cities.filter { isFavorite(cityId: $0.id) }
+    }
+}
---
+++ UalaCities/Core/Domain/UseCases/SearchCitiesUseCase.swift
@@ -0,0 +1,33 @@
+//
+//  SearchCitiesUseCase.swift
+//  UalaCities
+//
+//  Created by MATIAS BATTITI on 14/05/2025.
+//
+
+import Foundation
+
+/// Use case for searching cities by prefix
+protocol SearchCitiesUseCase {
+    /// Executes the search with the given prefix
+    func execute(prefix: String) -> [City]
+    
+    /// Loads all cities from the repository
+    func loadAllCities() async throws -> [City]
+}
+
+class DefaultSearchCitiesUseCase: SearchCitiesUseCase {
+    private let repository: CitiesRepository
+    
+    init(repository: CitiesRepository) {
+        self.repository = repository
+    }
+    
+    func execute(prefix: String) -> [City] {
+        repository.searchCities(prefix: prefix)
+    }
+    
+    func loadAllCities() async throws -> [City] {
+        try await repository.getAllCities()
+    }
+}
