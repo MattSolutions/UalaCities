d542962833225669907e21c4cfb22e67489aa154
mbattiti <A05331@bh.com.ar>
2025-05-14 15:13:28 -0300

    Add City and Coordinate models

---
+++ UalaCities/Core/Domain/Models/City.swift
@@ -0,0 +1,45 @@
+//
+//  City.swift
+//  UalaCities
+//
+//  Created by MATIAS BATTITI on 14/05/2025.
+//
+import Foundation
+import CoreLocation
+
+struct City: Identifiable, Equatable, Hashable {
+    let id: Int
+    let name: String
+    let country: String
+    let coordinates: Coordinate
+    
+    var displayName: String {
+        "\(name), \(country)"
+    }
+    
+    static func == (lhs: City, rhs: City) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct Coordinate: Equatable, Hashable {
+    let latitude: Double
+    let longitude: Double
+    
+    var clLocation: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static func == (lhs: Coordinate, rhs: Coordinate) -> Bool {
+        lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(latitude)
+        hasher.combine(longitude)
+    }
+}
